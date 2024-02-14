7bb6f6c62f1447a3d634b046861fd8e763783197
dzyfer <thegursh1@gmail.com>
2024-02-14 10:49:13 -0800

    Time: 41 ms (85.71%) | Memory: 12.3 MB (70.99%) - LeetSync

--- 344-reverse-string/reverse-string.c
+++ 344-reverse-string/reverse-string.c
@@ -1,10 +1,4 @@
 void reverseString(char* s, int sSize) {
-// char* s1[sSize];
-// int count = 0;
-// for (int i = sSize-1; i > 0; i--) {
-//     s1[count] = s[i];
-//     count++;
-// }
 int i = 0;
 int j = sSize-1;
     while (i < j) {
